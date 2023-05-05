d36750e0a7629807e7fc07659ca9158a6324c3e2
marcogara <103924910+marcogara@users.noreply.github.com>
2023-05-05 15:57:49 +0200

    Tag03 OOP1

---
+++ Projekt_OP1/AufgabeTag03/src/DrawSquare.java
@@ -0,0 +1,15 @@
+public class DrawSquare {
+
+
+    public static void drawsquare(int a) {
+
+        for(int i=0;i<a;i++)
+        {
+            for(int j=0;j<a;j++)
+            {
+                System.out.print(" # ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Projekt_OP1/AufgabeTag03/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // DrawSquare drawsquare = new DrawSquare();
+
+        // DrawSquare.drawsquare(10);
+
+        // Taschenrechner taschenrechner = new Taschenrechner();
+
+        // Taschenrechner.taschenrechner();
+
+        HuderteTueren spiel = new HuderteTueren();
+
+        HuderteTueren.spiel();
+
+
+        }
+    }
+
+
+
