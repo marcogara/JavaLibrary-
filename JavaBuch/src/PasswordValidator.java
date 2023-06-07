5ac93dfea30651d1a4e75ba678c84786c1d9b648
marcogara <103924910+marcogara@users.noreply.github.com>
2023-06-07 22:50:57 +0200

    Update PasswordValidator.java

--- JavaBuch/src/PasswordValidator.java
+++ JavaBuch/src/PasswordValidator.java
@@ -4,27 +4,16 @@ public class PasswordValidator {
 
     private static final int MIN_PASSWORD_LENGTH = 8;
     private static final String REQUIRED_CHARACTER_TYPES = ".*[a-z].*[A-Z].*[0-9].*";
-    private static String password;
 
-    public static boolean isValidInput(String s) {
-        if (s.length() < MIN_PASSWORD_LENGTH) {
-            return false;
-        }
-
-        return s.matches(REQUIRED_CHARACTER_TYPES);
+    public static boolean isValidPassword(String password) {
+        return password.length() >= MIN_PASSWORD_LENGTH && password.matches(REQUIRED_CHARACTER_TYPES);
     }
 
-    public static void setPassword(String p) {
-        StringBuilder message = new StringBuilder();
-        if (isValidInput(p)) {
-            password = p;
-            message.append("Your password is accepted: ").append(p);
-        } else {
-            password = "";
-            message.append("Your password is not accepted: ").append(p);
-        }
+    public static void setPassword(String password) {
+        String validityMessage = isValidPassword(password) ? "valid" : "invalid";
+        String message = "Your password is " + validityMessage + ": " + password;
 
-        JOptionPane.showMessageDialog(null, message.toString());
+        JOptionPane.showMessageDialog(null, message);
     }
 
     public static void main(String[] args) {
