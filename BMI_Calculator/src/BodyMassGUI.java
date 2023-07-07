5220a678b46bda51d000782a85eeec32798e0d84
marcogara <103924910+marcogara@users.noreply.github.com>
2023-07-07 15:08:20 +0200

    Add files via upload

---
+++ BMI_Calculator/src/BodyMassGUI.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BodyMassGUI extends JFrame {
+    private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button;
+    private JLabel outputLabel;
+
+    public BodyMassGUI() {
+        setTitle("Simple Panel");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 200);
+        setLayout(new BorderLayout());
+
+        // Create the input panel
+        panel = new JPanel();
+        panel.setLayout(new FlowLayout());
+
+        JLabel label1 = new JLabel("Input 1:");
+        textField1 = new JTextField(10);
+
+        JLabel label2 = new JLabel("Input 2:");
+        textField2 = new JTextField(10);
+
+        panel.add(label1);
+        panel.add(textField1);
+
+        panel.add(label2);
+        panel.add(textField2);
+
+        add(panel, BorderLayout.NORTH);
+
+        button = new JButton("Calculate");
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                int num1=Integer.parseInt(textField1.getText());
+                int num2=Integer.parseInt(textField2.getText());
+                int sum=num1+num2;
+
+                outputLabel.setText("Sum: "+sum);
+            }
+
+        });
+
+        add(button,BorderLayout.CENTER);
+
+        outputLabel=new JLabel();
+        add(outputLabel,BorderLayout.SOUTH);
+
+        setDynamicLocation();
+    }
+
+
+
+    private void setDynamicLocation() {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        int frameWidth = panel.getWidth();
+        int frameHeight = panel.getHeight();
+
+        int x = (screenSize.width - frameWidth) / 2;
+        int y = (screenSize.height - frameHeight) / 2;
+
+        setLocation(x, y);
+    }
+
+
+}
--- BMI_Calculator/src/Main.java
+++ BMI_Calculator/src/Main.java
@@ -1,8 +1,11 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
+
+
+/*
         System.out.println("Hello and welcome!");
 
         System.out.println("Please enter your size in meter: (format ->  #,##) ...  ");
@@ -17,7 +20,20 @@ public class Main {
 
         System.out.println("your BMI is: "+ bmi.getBmi());
 
+
+ */
+        public static void main(String[] args) {
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    BodyMassGUI frame=new BodyMassGUI();
+                    frame.setVisible(true);
+                }
+            });
         }
 
 
     }
+
+
+
+
