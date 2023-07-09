ad30a89874371129107f910fbbc6e5908047529d
GoodCodeReader <askoda804@gmail.com>
2023-07-09 13:02:54 +0400

    Refactor the code formatting in the BMI_calculator package to enhance consistency and readability throughout the files.
    - Removed unnecessary "this" prefix before class members
    - Updated the setBmi() method to calculate the BMI using the provided size and weight values.
    - Align code blocks and statements for improved visual structure.
    - Remove trailing whitespaces and unnecessary blank lines.

--- BMI_Calculator/src/BodyMassCalculator.java
+++ BMI_Calculator/src/BodyMassCalculator.java
@@ -1,22 +1,19 @@
 public class BodyMassCalculator {
-    private double groesse ;
-    private double gewicht ;
+    private double groesse;
+    private double gewicht;
     private double bmi;
 
-
     public BodyMassCalculator(double size, double weight) {
-
-        this.groesse = size;
-        this.gewicht = weight;
+        groesse = size;
+        gewicht = weight;
+        setBmi();
     }
 
-    public double getBmi ()
-    {
-
-        this.bmi=this.gewicht / (this.groesse*this.groesse);
-
-        return this.bmi;
+    private void setBmi() {
+        bmi = gewicht / (groesse * groesse);
     }
 
-
+    public double getBmi() {
+        return bmi;
+    }
 }
