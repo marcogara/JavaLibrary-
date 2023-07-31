2bbbe75ecdd69588f873eca3424e8ee7c2e7cada
marcogara <103924910+marcogara@users.noreply.github.com>
2023-07-31 14:21:05 +0200

    Update Main.java

--- Projekt_OP1/AufgabeTag02/src/Main.java
+++ Projekt_OP1/AufgabeTag02/src/Main.java
@@ -1,5 +1,3 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
 
@@ -12,14 +10,11 @@ public class Main {
 
         QuerSumme test1 = new QuerSumme();
 
-
         System.out.println("QuerSumme von 123435 ist : "+ test1.quersumme(123435));
 
         // aufgabe 2
 
         Aufgabe2 hallo = new Aufgabe2();
-
-
 
         Aufgabe2.G_mal_Hallo();
         System.out.println();
@@ -27,18 +22,5 @@ public class Main {
         int l=4;
         for (int i=0; i<l;i++)
             System.out.println("Welt");
-
-
-
-        // Press Alt+Eingabe with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        // System.out.printf("Hello and welcome!");
-
-        // Press Umschalt+F10 or click the green arrow button in the gutter to run the code.
-        // for (int i = 1; i <= 5; i++) {
-
-        // Press Umschalt+F9 to start debugging your code. We have set one breakpoint
-        // for you, but you can always add more by pressing Strg+F8.
-        // System.out.println("i = " + i);
         }
     }
