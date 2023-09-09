b313ba2b7aea910fd662145b781f4e11aa741c32
marcogara <103924910+marcogara@users.noreply.github.com>
2023-09-09 17:32:22 +0200

    Update Main.java

--- Projekt_OP1/AufgabeTag02/src/Main.java
+++ Projekt_OP1/AufgabeTag02/src/Main.java
@@ -2,18 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         // aufgabe 1
-
         CheckGeradeUngerade test = new CheckGeradeUngerade();
         System.out.println( test.checkgerade(2549));
 
         // Aufgabe Quersumme
-
         QuerSumme test1 = new QuerSumme();
 
         System.out.println("QuerSumme von 123435 ist : "+ test1.quersumme(123435));
 
         // aufgabe 2
-
         Aufgabe2 hallo = new Aufgabe2();
 
         Aufgabe2.G_mal_Hallo();
